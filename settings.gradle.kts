75118e279d2cc560319e44fd74e387d1af786767
svetlana.isakova <svetlana.isakova@jetbrains.com>
2022-09-08 07:57:04 +0200

    Created project

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KotlinMultiplatformTemplate"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/template/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.template
+
+expect class Platform() {
+    val platform: String
+}
